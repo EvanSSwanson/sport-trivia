a8cc2018e00349a66c5fbd4cd064ba271202deea
Evan Swanson <evan.sundelius@gmail.com>
2023-07-24 11:18:27 -0500

    Create home page component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from '../../logo.svg';
+import { Route, Routes, NavLink } from "react-router-dom"
 import Header from '../Header/Header'
+import HomePage from '../Homepage/Homepage';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './Homepage.css';
+
+const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default HomePage;
